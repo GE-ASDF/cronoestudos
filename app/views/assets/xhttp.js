e72a5e3e519a0e755df7eb56c8a7e3432ea67c5b
Anderson Souza <andersonsouza007@live.com>
2022-07-26 13:49:57 -0300

    Usando ajax para cadastrar usuário na página de login

--- app/views/assets/xhttp.js
+++ app/views/assets/xhttp.js
@@ -1,5 +1,15 @@
 var xhttp = new XMLHttpRequest;
 
+function xmlHttpGet(url, callback, parameters=''){
+
+    xhttp.onreadystatechange = callback;
+
+    xhttp.open("GET", url+parameters,true)
+
+    xhttp.send();
+
+}
+
 function xmlHttpPost(url, callback, parameters = ''){
     
     xhttp.onreadystatechange = callback;
